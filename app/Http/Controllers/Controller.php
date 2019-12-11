a49adf714afeda6569a65e603670ee0e9ad9d3c8
kailaixin <1124802717@qq.com>
2019-12-11 16:53:46 +0800

    first 1903

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -9,6 +9,6 @@ use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;git commit -m "first commit"
 }
-get
+
